db6de4ffd3aceb203fe6f377e405da13c9340c06
Al Fahad <alfahad@zeptosystems.com>
2022-01-04 16:04:42 +0500

    mongoose added

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
 
 import express from 'express';
-import {Application, Request, Response} from "express";
+import { Application, Request, Response } from "express";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
 const app: Application = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 
-app.get( "/", ( req: Request, res: Response ) => {
-    res.send( "Working :)" );
+app.get("/", (req: Request, res: Response) => {
+    res.send("Working :)");
 });
 
 app.listen(port, () => {
-  return console.log(`Server is listening at http://localhost:${port}`);
+    return console.log(`Server is listening at http://localhost:${port}`);
 });
