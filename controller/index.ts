3cc5fe0a56fd74979a4d72c2a317d4ddb2647993
Al Fahad <alfahad@zeptosystems.com>
2022-01-05 13:42:11 +0500

    dummy add functionality added in book controller

---
+++ controller/index.ts
@@ -0,0 +1,7 @@
+import { Request, Response, NextFunction } from "express";
+
+export class BookController{
+    static async add(req: Request, res: Response, next: NextFunction){
+        res.status(200).send({"Status": "Added Sucessfully!"})
+    }
+}
