30389064ff92f3b9b38847921cc4f4119f2cae2b
Al Fahad <alfahad@zeptosystems.com>
2022-01-05 13:43:47 +0500

    dummy update functionality in book controller

--- controller/index.ts
+++ controller/index.ts
@@ -4,4 +4,7 @@ export class BookController{
     static async add(req: Request, res: Response, next: NextFunction){
         res.status(200).send({"Status": "Added Sucessfully!"})
     }
+    static async update(req: Request, res: Response, next: NextFunction){
+        res.status(200).send({"Status": "Updated Sucessfully!"})
+    }
 }
