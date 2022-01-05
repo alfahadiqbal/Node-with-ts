4c83bdb483798508ed2e5d361851f4e2ad26bed6
Al Fahad <alfahad@zeptosystems.com>
2022-01-05 13:28:26 +0500

    index routes added

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import { Application, Request, Response } from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import { router } from '../routes/index';
 const app: Application = express();
 const port = 3000;
 
@@ -20,6 +21,7 @@ app.get("/", (req: Request, res: Response) => {
     res.send("Working :)");
 });
 
+app.use('/', router);
 app.listen(port, () => {
     return console.log(`Server is listening at http://localhost:${port}`);
 });
