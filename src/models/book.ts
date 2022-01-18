09a7d9805cbdac39b9589d9cc9397fb4be4ecb17
Al Fahad <alfahad@zeptosystems.com>
2022-01-18 14:46:51 +0500

    Book Schema added

--- src/models/book.ts
+++ src/models/book.ts
@@ -1,7 +1,13 @@
+import { Schema } from 'mongoose'
+
 interface Book {
     name: string,
     author: string
     
 }
+const bookSchema = new Schema<Book>({
+    name: {type: String, required: true},    
+    author: {type: String, required: true}    
+})
 
 export { Book };
