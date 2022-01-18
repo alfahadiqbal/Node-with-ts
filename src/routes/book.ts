254c3f4975f39178dc8967ccce06ec50c9dea1ae
Al Fahad <alfahad@zeptosystems.com>
2022-01-18 19:42:47 +0500

    customer book route snippet added

---
+++ src/routes/book.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { BookModel } from "../models/book";
+import { API_TYPE } from "../models/types";
+import { commonRoute } from "./common-route";
+
+// If you want to create some routes using common functionlity and for rest you want to write custom
+// functionality you can do that by specifing required routes in array and then you can write 
+// rest of routes yourself
+
+export const bookRouter: Router = commonRoute(BookModel, [API_TYPE.GET_ALL])
