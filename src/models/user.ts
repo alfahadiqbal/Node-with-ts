416ed9e092c996b568f83e41cf8541aee161c67b
Al Fahad <alfahad@zeptosystems.com>
2022-01-18 19:25:39 +0500

    User schema added

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,3 +1,4 @@
+import { Schema } from 'mongoose'
 
 interface User {
     firstName: string,
@@ -5,5 +6,11 @@ interface User {
     dateOfBirth: Date
     
 }
+
+const userSchema = new Schema<User>({
+    firstName: {type: String, required: true},    
+    lastName: {type: String, required: true},
+    dateOfBirth: {type: Date, required: false}    
+})
 
 export { User };
