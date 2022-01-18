e740b5aeec58bc79d455bc78009717c78456f305
Al Fahad <alfahad@zeptosystems.com>
2022-01-18 19:57:02 +0500

    Add book in mongo db - custom controller

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,8 +1,21 @@
 import { Request, Response, NextFunction } from "express";
+import { BookModel } from "../models/book";
 
 export class BookController{
     static async add(req: Request, res: Response, next: NextFunction){
-        res.status(200).send({"Status": "Added Sucessfully!"})
+
+        const query = BookModel.findById(req.params.id).exec()
+        query.then((resource) => {
+
+            if (!resource) {
+                res.status(200).send({"Status": "Added Sucessfully"})
+            }
+
+            return res.json(resource)
+        })
+        .catch((err) => {
+            res.status(200).send({"Status": "Error Occured"})
+        })
     }
     static async update(req: Request, res: Response, next: NextFunction){
         res.status(200).send({"Status": "Updated Sucessfully!"})
