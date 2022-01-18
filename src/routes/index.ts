9bf3c015a7f3061a8a09e8e5fdbdb3aa5c783ee8
Al Fahad <alfahad@zeptosystems.com>
2022-01-18 18:22:51 +0500

    Book common route added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,8 @@
 import { Router} from 'express'
+import { BookModel } from '../models/book';
+import { commonRoute } from './common-route';
 
 export const router: Router = Router();
+
+// This is how you can add all routes with single line of code :)
+router.use('', commonRoute(BookModel))
