0be726e36e7f8060110ab29439f5008fc3fdc122
Al Fahad <alfahad@zeptosystems.com>
2022-01-18 18:02:57 +0500

    common GetAll route added

--- src/routes/common-route.ts
+++ src/routes/common-route.ts
@@ -1,8 +1,18 @@
 import { Router, Request, Response } from 'express'
 import { Model } from 'mongoose';
+import { getAll } from '../controller/common-controller';
 
 export const commonRoute = (model: Model<any>, path?: string): Router => {
     const router: Router = Router();
-    // TODO: Add common routes here
-   return router
+    const urlPath = (path)? path: model.modelName.toLowerCase()
+
+    router.get(
+        `/${urlPath}s`,
+        // middleware.authenticate,
+        (request, response, next) => {
+            getAll(request, response, next, model)
+        }
+    )
+
+    return router
 }
