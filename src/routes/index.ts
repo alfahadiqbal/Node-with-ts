1a9a7a96a8441128d473fc392e106b8a08ae3a18
Al Fahad <alfahad@zeptosystems.com>
2022-02-04 18:37:34 +0500

    Auth routes added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router} from 'express'
 import { UserModel } from '../models/user';
+import authenticationRouter from './auth';
 import { commonRoute } from './common-route';
 
 export const router: Router = Router();
 
 // This is how you can add all routes with single line of code :)
 router.use('', commonRoute(UserModel))
+router.use('', authenticationRouter)
