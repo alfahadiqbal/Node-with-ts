e7c0810a32efd46aeab13e2ff22251b6e41788d3
Al Fahad <alfahad@zeptosystems.com>
2022-02-04 10:15:46 +0500

    typo fixed

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -2,13 +2,16 @@ import { NextFunction, Request, Response, Router } from "express";
 // import AuthController from "../controllers/AuthController";
 // import { checkJwt } from "../middlewares/checkJwt";
 
-const router = Router();
+const authenticationRouter = Router();
+
 //Login route
-router.post("/login", (req: Request, res: Response, nexr:NextFunction)=>{
+authenticationRouter.post("/login", (req: Request, res: Response, next:NextFunction)=>{
     res.status(200).send("/login success")
 });
+
 //Change my password
-router.post("/change-password", (req: Request, res: Response, nexr:NextFunction)=>{
+authenticationRouter.post("/change-password", (req: Request, res: Response, next:NextFunction)=>{
     res.status(200).send("change-password success")
 });
-export default router;
+
+export default authenticationRouter;
