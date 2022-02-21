87d6d6435faf7151223f5bd4774a7f961cc3ca2d
Al Fahad <alfahad@zeptosystems.com>
2022-02-21 12:42:12 +0500

    login api modified

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -4,27 +4,27 @@ import * as jwt from "jsonwebtoken";
 
 class AuthController {
     static login = async (req: Request, res: Response, next: NextFunction)=>{
-        let [username, password] = req.body;
-
+        let username = req.body.username;
+        let password = req.body.password;
         if(!(username && password)) {
             res.status(400).send("Please enter Username and passowrd")
         }
-        const query = UserModel.find({username: username, password: password}).exec()
+        const query = UserModel.findOne({username: username, password: password}).exec()
             query.then((user) => {
                 if (!user) {
                     return next("Invalid Username or Password")
                 }
 
-                // //Sing JWT, valid for 1 hour
-                // const token = jwt.sign(
-                //     { userId: user._id, username: user.username },
-                //     'config.jwtSecret',
-                //     { expiresIn: "1h" }
-                // );
-
-                // //Send the jwt in the response
-                // res.send(token);
-                return res.json("Login Sucessful")
+                //Sing JWT, valid for 1 hour
+                const token = jwt.sign(
+                    { userId: user._id, username: user.username },
+                    'config.jwtSecret',
+                    { expiresIn: "1h" }
+                );
+                    
+                // Send the jwt in the response
+                return res.send({"loginToken": token});
+                // return res.json("Login Sucessful")
             })
             .catch((err) => {
                 // send the error to the error handler
